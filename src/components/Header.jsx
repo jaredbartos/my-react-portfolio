090d73e6f79b96caa84a8a2fd0ead7f61df4cda5
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-01-22 16:08:40 -0600

    Add logic for active links in nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ export default function Header() {
   return (
     <Navigation
     links={[
-      <Link key={1} className="nav-link" to="/">About Me</Link>,
-      <Link key={2} className="nav-link" to="/portfolio">Portfolio</Link>,
-      <Link key={3} className="nav-link" to="/contact">Contact</Link>,
-      <Link key={4} className="nav-link" to="/resume">Resume</Link>
+      <Link key={1} className={`nav-link ${document.location.pathname === '/' && 'active'}`} to="/">About Me</Link>,
+      <Link key={2} className={`nav-link ${document.location.pathname === '/portfolio' && 'active'}`} to="/portfolio">Portfolio</Link>,
+      <Link key={3} className={`nav-link ${document.location.pathname === '/contact' && 'active'}`} to="/contact">Contact</Link>,
+      <Link key={4} className={`nav-link ${document.location.pathname === '/resume' && 'active'}`} to="/resume">Resume</Link>
     ]}
     />
   )
