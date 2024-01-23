4bbb689a4c38d9dbfc04cbe2b2aa01376ca18a78
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-01-23 11:17:40 -0600

    Add new data.js file to hold needed data for rendering

---
+++ src/assets/js/data.js
@@ -0,0 +1,7 @@
+// Array to use for nav links
+export const links = [
+  { key: 1, title: 'About Me', path: '/' },
+  { key: 2, title: 'Portfolio', path: '/portfolio' },
+  { key: 3, title: 'Contact', path: '/contact' },
+  { key: 4, title: 'Resume', path: '/resume' }
+];
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,36 +1,33 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+// Import link data
+import { links } from '../assets/js/data'
 
 export default function Navigation() {
-  // Array to use for nav links
-  const links = [
-    { key: 1, title: 'About Me', path: '/' },
-    { key: 2, title: 'Portfolio', path: '/portfolio' },
-    { key: 3, title: 'Contact', path: '/contact' },
-    { key: 4, title: 'Resume', path: '/resume' }
-  ]
-
   // Declare useState variables for the active location path and set value to initial path
   const [activePath, setActivePath] = useState(document.location.pathname);
 
+  // Map through link data to create link elements for use in navigation
+  const linkElements = links.map((link) => {
+    return (
+      <Link
+        key={link.key}
+        to={link.path}
+        // Set onclick behavior to change activePath state to link path
+        onClick={() => setActivePath(link.path)}
+        // Check if activePath is equal to the path of the link
+        // If so, add 'active' class for bootstrap styling
+        className={activePath === link.path ? 'nav-link active' : 'nav-link'}
+      >
+        {link.title}
+      </Link>
+    );
+  });  
+
   return (
     <ul className="navbar-nav">
-      {/* Map through links array to render page links */}
-      {links.map((link) => {
-        return (
-          <Link
-            key={link.key}
-            to={link.path}
-            // Set onclick behavior to change activePath state to link path
-            onClick={() => setActivePath(link.path)}
-            // Check if activePath is equal to the path of the link
-            // If so, add 'active' class for bootstrap styling
-            className={activePath === link.path ? 'nav-link active' : 'nav-link'}
-          >
-            {link.title}
-          </Link>
-        );
-      })}
+      {/* Insert links into nav */}
+      {linkElements}
     </ul>
   );
 }
