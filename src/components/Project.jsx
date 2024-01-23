98e1d463dd474db746b0808f5f498e1ff3beb706
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-01-23 11:04:40 -0600

    Add project component

---
+++ src/components/Project.jsx
@@ -0,0 +1,12 @@
+export default function Project({ image, title, deployedLink, githubLink }) {
+  return (
+    <div className="card">
+      <img src={image} className="card-img-top" alt={`${title} Screenshot`} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <a href={deployedLink} className="btn btn-primary" target="_blank" rel="noreferrer">App</a>
+        <a href={githubLink} className="btn btn-primary" target="_blank" rel="noreferrer">GitHub Repo</a>
+      </div>
+    </div>
+  );
+}
