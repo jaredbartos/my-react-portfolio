40223479430f7647c2c25131d921a8a1e61fb0f3
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-01-24 19:19:03 -0600

    Add skills array in data.js to use on resume page

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -48,3 +48,86 @@ export const projects = [
     githubLink: 'https://www.google.com'
   },
 ];
+
+export const skills = [
+  {
+    id: 1,
+    name: 'HTML',
+    category: 'Languages and Concepts'
+  },
+  {
+    id: 2,
+    name: 'CSS',
+    category: 'Languages and Concepts'
+  },
+  {
+    id: 3,
+    name: 'JavaScript ES6+',
+    category: 'Languages and Concepts'
+  },
+  {
+    id: 4,
+    name: 'jQuery',
+    category: 'JavaScript Tools'
+  },
+  {
+    id: 5,
+    name: 'React.js',
+    category: 'JavaScript Tools'
+  },
+  {
+    id: 6,
+    name: 'Node.js',
+    category: 'JavaScript Tools'
+  },
+  {
+    id: 7,
+    name: 'MySQL',
+    category: 'Databases'
+  },
+  {
+    id: 8,
+    name: 'Sequelize.js',
+    category: 'JavaScript Tools'
+  },
+  {
+    id: 9,
+    name: 'NoSQL',
+    category: 'Languages and Concepts'
+  },
+  {
+    id: 10,
+    name: 'MongoDB',
+    category: 'Databases'
+  },
+  {
+    id: 11,
+    name: 'Progressive Web Applications (PWAs)',
+    category: 'Languages and Concepts'
+  },
+  {
+    id: 12,
+    name: 'SQL',
+    category: 'Languages and Concepts'
+  },
+  {
+    id: 13,
+    name: 'Express.js',
+    category: 'JavaScript Tools'
+  },
+  {
+    id: 14,
+    name: 'Mongoose.js',
+    category: 'JavaScript Tools'
+  },
+  {
+    id: 15,
+    name: 'APIs',
+    category: 'Languages and Concepts'
+  },
+  {
+    id: 16,
+    name: 'Handlebars.js',
+    category: 'JavaScript Tools'
+  }
+];
