16340f37fba588b5130a440ad65438c15a1a6f22
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-01-25 19:14:17 -0600

    Delete unused create-react-app files

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,6 @@ import PortfolioPage from './pages/PortfolioPage';
 import ContactPage from './pages/ContactPage';
 import ResumePage from './pages/ResumePage';
 
-import reportWebVitals from './reportWebVitals';
 // Import React Router modules
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
@@ -51,8 +50,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
