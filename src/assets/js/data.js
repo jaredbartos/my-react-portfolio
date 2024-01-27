f8bedd90bc2704c25a1f994e1e5f45b6bb7a734d
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-01-27 16:31:22 -0600

    Add delay to project cards

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -17,36 +17,42 @@ export const links = [
 // Array to use for rendering projects on portfolio page
 export const projects = [
   {
+    id: 1,
     title: 'The Tech Blog',
     image: techBlogScreenshot,
     deployedLink: 'https://whispering-headland-36327-fcf93ff6f15e.herokuapp.com/',
     githubLink: 'https://github.com/jaredbartos/tech-blog'
   },
   {
+    id: 2,
     title: 'J.A.T.E. Text Editor',
     image: jateScreenshot,
     deployedLink: 'https://text-editor-jaredbartos.onrender.com/',
     githubLink: 'https://github.com/jaredbartos/text-editor'
   },
   {
+    id: 3,
     title: 'Note Taker',
     image: noteTakerScreenshot,
     deployedLink: 'https://note-taker-jaredbartos.onrender.com/',
     githubLink: 'https://github.com/jaredbartos/note-taker'
   },
   {
+    id: 4,
     title: 'GameDex',
     image: gameDexScreenshot,
     deployedLink: 'https://gamedex-project.onrender.com/',
     githubLink: 'https://github.com/jaredbartos/gamedex'
   },
   {
+    id: 5,
     title: 'Weather Dashboard',
     image: weatherDashboardScreenshot,
     deployedLink: 'https://jaredbartos.github.io/weather-dashboard',
     githubLink: 'https://github.com/jaredbartos/weather-dashboard'
   },
   {
+    id: 6,
     title: 'Code Quiz',
     image: codeQuizScreenshot,
     deployedLink: 'https://jaredbartos.github.io/code-quiz',
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -5,9 +5,17 @@ import Project from '../components/Project';
 
 export default function PortfolioPage() {
   // Map through project data to create project cards
-  const projectCards = projects.map((project, index) => {
+  const projectCards = projects.map((project) => {
+    // Set variable for amount of animation delay so it increases
+    // for each project card
+    const delay = project.id * 0.1;
+
+    const styles = {
+      animationDelay: `${delay}s`
+    };
+
     return (
-      <div key={index} className="col-lg-6">
+      <div key={project.id} style={styles} className="col-lg-6 from-bottom">
         <Project
           {...project}
         />
@@ -18,7 +26,7 @@ export default function PortfolioPage() {
   return (
     <div className="container-fluid from-bottom">
       <div className="row mb-5">
-        <h2 className="col text-center merriweather-regular h4 ">Portfolio</h2>
+        <h2 className="col text-center merriweather-regular h4">Portfolio</h2>
       </div>
       <div className="container">
         <div className="row d-flex flex-wrap justify-content-center">
