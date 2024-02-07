85f7f3bdfa676864eb0d806fa4204c0059b5cd17
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-02-06 21:16:23 -0600

    Fix bug in server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const { transporter } = require('./config/config');
 const PORT = process.env.PORT || 3001;
 const app = express();
