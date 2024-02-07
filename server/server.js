b1794bb02c0cc4fdef74612068ed09011412c7f4
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-02-06 21:12:20 -0600

    Troubleshoot render deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
+
 app.listen(PORT, () =>  {
   transporter.verify((error) => {
     if (error) {
