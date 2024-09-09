526de8e80b5d7300af1156665b2dd6c5ada7f19c
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-09-09 16:02:14 -0500

    Add icons in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,39 +1,46 @@
+import { FaGithub } from 'react-icons/fa6';
+import { FaLinkedin } from 'react-icons/fa6';
+import { SiCodesignal } from 'react-icons/si';
+
 export default function Footer() {
   return (
-    <div className='container-fluid from-bottom'>
-      <div className='row mt-5'>
-        <div className='col d-flex justify-content-center'>
+    <div className="container-fluid from-bottom">
+      <div className="row mt-5">
+        <div className="col d-flex justify-content-center">
           <a
-            className='mx-3'
-            href='https://github.com/jaredbartos'
-            target='_blank'
-            rel='noreferrer'
-            title='GitHub Profile'
+            className="mx-3 fs-1"
+            style={{ color: '#6e5494' }}
+            href="https://github.com/jaredbartos"
+            target="_blank"
+            rel="noreferrer"
+            title="GitHub Profile"
           >
-            GitHub
+            <FaGithub />
           </a>
           <a
-            className='mx-3'
-            href='https://www.linkedin.com/in/jared-bartos-726762272'
-            target='_blank'
-            rel='noreferrer'
-            title='LinkedIn Profile'
+            className="mx-3 fs-1"
+            style={{ color: '#0a66c2' }}
+            href="https://www.linkedin.com/in/jared-bartos-726762272"
+            target="_blank"
+            rel="noreferrer"
+            title="LinkedIn Profile"
           >
-            LinkedIn
+            <FaLinkedin />
           </a>
           <a
-            className='mx-3'
-            href='https://app.codesignal.com/profile/jaredbartos'
-            target='_blank'
-            rel='noreferrer'
-            title='CodeSignal Profile'
+            className="mx-3 fs-1"
+            style={{ color: '#3a73cf' }}
+            href="https://app.codesignal.com/profile/jaredbartos"
+            target="_blank"
+            rel="noreferrer"
+            title="CodeSignal Profile"
           >
-            CodeSignal
+            <SiCodesignal />
           </a>
         </div>
       </div>
-      <div className='row'>
-        <p className='col text-center'>&copy; 2024 - Jared Bartos</p>
+      <div className="row">
+        <p className="col text-center">&copy; 2024 - Jared Bartos</p>
       </div>
     </div>
   );
